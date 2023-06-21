5030fb06c7189cd06feaf9d0d985bde816d61b5d
Juan Pablo <82411091+Juampy-cabj0@users.noreply.github.com>
2023-06-20 22:25:16 -0300

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,28 @@
+/* const miBoton = document.getElementById('miBoton'); 
+
+function handleClick() {
+    alert('¡Hola! Has hecho click en el botón.');
+}
+
+miBoton.addEventListener('click', handleClick); */
+
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/;
+  return regex.test(email);
+}
+function validateForm() {
+  const emailInput = document.getElementById("email");
+  const email = emailInput.value;
+  if (!validateEmail(email)) {
+    alert("Por favor ingrese un correo electrónico válido.");
+  } else {
+    alert("Correo electrónico enviado correctamente.");
+  }
+}
+
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  validateForm();
+});
